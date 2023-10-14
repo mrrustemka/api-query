b067aa8ce8fe26c471928c8d51d6f47ed84f8f5e
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-10-14 14:56:33 +0300

    add-apollo-client-to-repo-page

--- src/api/starRepo.ts
+++ src/api/starRepo.ts
@@ -1,22 +1,13 @@
-export const STAR_REPO = `
-mutation ($repoId: ID!) {
-    addStar (input: {starrableId: $repoId}) {
-        starrable {
-            stargazers {
-                totalCount
-            }
-        }
-    }
-}`;
+import { gql } from "@apollo/client/core";
 
-export async function starRepo(repoId: string) {
-  const response = await fetch(process.env.REACT_APP_GITHUB_URL!, {
-    method: "POST",
-    body: JSON.stringify({ query: STAR_REPO, variables: { repoId } }),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `bearer ${process.env.REACT_APP_GITHUB_PAT}`,
-    },
-  });
-  await response.json();
-}
+export const STAR_REPO = gql`
+  mutation ($repoId: ID!) {
+    addStar(input: { starrableId: $repoId }) {
+      starrable {
+        stargazers {
+          totalCount
+        }
+      }
+    }
+  }
+`;
--- src/repoPage/RepoPage.tsx
+++ src/repoPage/RepoPage.tsx
@@ -1,42 +1,43 @@
 import { useState } from "react";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { getRepo } from "../api/getRepo";
-import { starRepo } from "../api/starRepo";
-import { RepoData, SearchCriteria } from "../api/types";
+import { useLazyQuery, useMutation, useApolloClient } from "@apollo/client";
+import { GET_REPO } from "../api/getRepo";
+import { STAR_REPO } from "../api/starRepo";
+import { SearchCriteria } from "../api/types";
 import { SearchRepoForm } from "./SearchRepoForm";
 import { FoundRepo } from "./FoundRepo";
 import { StarRepoButton } from "./StarRepoButton";
 
 export function RepoPage() {
-  const [SearchCriteria, setSearchCriteria] = useState<
+  const [searchCriteria, setSearchCriteria] = useState<
     SearchCriteria | undefined
   >();
-  const { data } = useQuery(
-    ["repo", SearchCriteria],
-    () => getRepo(SearchCriteria as SearchCriteria),
-    { enabled: SearchCriteria !== undefined }
-  );
-  const queryClient = useQueryClient();
-  const { mutate } = useMutation(starRepo, {
-    onSuccess: () => {
-      queryClient.setQueryData<RepoData>(["repo", SearchCriteria], (repo) => {
-        if (repo === undefined) {
-          return undefined;
-        }
-        return {
-          ...repo,
-          viewerHasStarred: true,
-        };
+  const [getRepo, { data }] = useLazyQuery(GET_REPO);
+  const queryClient = useApolloClient();
+  const [starRepo] = useMutation(STAR_REPO, {
+    onCompleted: () => {
+      queryClient.cache.writeQuery({
+        query: GET_REPO,
+        data: {
+          repository: {
+            ...data.repository,
+            viewerHasStarred: true,
+          },
+        },
+        variables: searchCriteria,
       });
     },
   });
+
   function handleSearch(search: SearchCriteria) {
+    getRepo({
+      variables: { ...search },
+    });
     setSearchCriteria(search);
   }
 
   function handleStarClick() {
     if (data) {
-      mutate(data.repository.id);
+      starRepo({ variables: { repoId: data.repository.id } });
     }
   }
 
